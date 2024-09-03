2d800c787cbe48a6756f1fe56cc34921b17bc3d4
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-02 21:19:12 -0500

    add create action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,19 @@ class HomesController < ApplicationController
     @homes = Home.all
     render :index
   end
+
+  def create
+    @home = Home.create(
+      description: params[:description],
+      year_built: params[:year_built],
+      square_feet: params[:square_feet],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+      floors: params[:floors],
+      availability: params[:availability],
+      address: params[:address],
+      price: params[:price]
+    )
+    render :show
+  end
 end
