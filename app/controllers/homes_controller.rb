9506bc0ed8e47ab828bffc21bf78a4ca4a065809
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-02 21:28:20 -0500

    add show, update, and delete actions

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -19,4 +19,31 @@ class HomesController < ApplicationController
     )
     render :show
   end
+
+  def show
+    @home = Home.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @home = Home.find_by(id: params[:id])
+    @home.update(
+      description: params[:description] || @home.description,
+      year_built: params[:year_built] || @home.year_built,
+      square_feet: params[:square_feet] || @home.square_feet,
+      bedrooms: params[:bedrooms] || @home.bedrooms,
+      bathrooms: params[:bathrooms] || @home.bathrooms,
+      floors: params[:floors] || @home.floors,
+      availability: params[:availability] || @home.availability,
+      address: params[:address] || @home.address,
+      price: params[:price] || @home.price
+    )
+    render :show
+  end
+
+  def delete
+    @home = Home.find_by(id: params[:id])
+    @home.destroy
+    render json: {result: "Home at #{@home.address} deleted successfully."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get "/homes" => "homes#index"
   post "/homes" => "homes#create"
-
+  get "/homes/:id" => "homes#show"
+  patch "/homes/:id" => "homes#update"
+  delete "/homes/:id" => "homes#delete"
   # Defines the root path route ("/")
   # root "posts#index"
 end
